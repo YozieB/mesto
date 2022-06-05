9382de114362282711f2302906c9298bdffd0960
yozieee <mrfreeze987@gmail.com>
2022-06-05 18:24:52 +0300

    refactor: Рефакторинг кода JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
 const editProfile = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
-const closePopupBtn = popup.querySelectorAll('.popup__close-btn');
+const closeButtons = document.querySelectorAll('.popup__close-btn');
 const author = document.querySelector('.profile__title');
 const job = document.querySelector('.profile__about');
-const formElement = document.querySelector('.popup__form');
-const bioName = formElement.querySelector('.popup__input_name');
-const bioJob = formElement.querySelector('.popup__input_job');
+const profileForm = document.querySelector('.popup__form');
+const profileName = profileForm.querySelector('.popup__input_name');
+const profileJob = profileForm.querySelector('.popup__input_job');
 const cardName = document.querySelector('.popup__input_place');
 const cardLink = document.querySelector('.popup__input_link');
-const popupBio = document.querySelector('.popup__bio');
-const popupCard = document.querySelector('.popup__card');
 const addCardBtn = document.querySelector('.profile__add-btn');
 const gallery = document.querySelector('.gallery');
 const initialCards = [{
@@ -37,28 +35,52 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-const popupImage = document.querySelector('.popup__image')
+const popupImage = document.querySelector('.image-popup');
+const popupProfile = document.querySelector('.profile-popup');
+const popupCard = document.querySelector('.card-popup');
+const cardForm = popupCard.querySelector('.popup__form');
+const bigPicture = popupImage.querySelector('.popup__image-pic');
+const bigPictureTitle = popupImage.querySelector('.popup__image-title')
 
 // Creating card
 const createCard = (name, link) => {
     const cardTemplate = document.querySelector('#card').content;
     const card = cardTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__title').textContent = name;
-    card.querySelector('.card__image').src = link;
-    card.querySelector('.card__heart').addEventListener('click', (e) => {
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const heart = card.querySelector('.card__heart');
+    const removeBtn = card.querySelector('.card__trash');
+
+    cardTitle.textContent = name;
+    cardImage.src = link;
+    cardImage.alt = name;
+
+    heart.addEventListener('click', (e) => {
         e.target.classList.toggle('card__heart_active');
     });
-    card.querySelector('.card__trash').addEventListener('click', (e) => {
+
+    removeBtn.addEventListener('click', (e) => {
         const closestCard = e.target.closest('.card');
         closestCard.remove();
     });
-    card.querySelector('.card__image').addEventListener('click', (e) => {
+
+    cardImage.addEventListener('click', (e) => {
         openPopup(popupImage);
-        popup.classList.add('popup_opened_image');
-        popupImage.querySelector('.popup__image-pic').src = e.target.src;
-        popupImage.querySelector('.popup__image-title').textContent = card.querySelector('.card__title').textContent;
+        bigPicture.src = link;
+        bigPictureTitle.textContent = name;
     });
+    
     return card
+}
+
+// Open popup
+const openPopup = (element) => {
+    element.classList.add('popup_opened');
+}
+
+// Close popup
+const closePopup = (element) => {
+    element.classList.remove('popup_opened');
 }
 
 // Adding start cards
@@ -67,32 +89,18 @@ initialCards.forEach(el => {
 })
 
 // Adding new card
-const popupCardForm = popupCard.querySelector('.popup__form');
-popupCardForm.addEventListener('submit', (e) => {
+cardForm.addEventListener('submit', (e) => {
     e.preventDefault();
     gallery.prepend(createCard(cardName.value, cardLink.value));
-    popupCardForm.reset();
-    closePopup();
+    cardForm.reset();
+    closePopup(popupCard);
 })
 
-// Open popup
-const openPopup = (element) => {
-    popup.classList.add('popup_opened');
-    element.classList.add('popup__item_opened');
-}
-
-// Close popup
-const closePopup = () => {
-    const popupElements = Array.from(popup.children);
-    popupElements.forEach(el => {
-        el.classList.remove('popup__item_opened');
+closeButtons.forEach(button => {
+    const popup = button.closest('.popup');
+    button.addEventListener('click', (e) => {
+        closePopup(popup)
     });
-    popup.classList.remove('popup_opened');
-    popup.classList.remove('popup_opened_image');
-}
-
-closePopupBtn.forEach(el => {
-    el.addEventListener('click', closePopup)
 })
 
 addCardBtn.addEventListener('click', () => {
@@ -100,14 +108,14 @@ addCardBtn.addEventListener('click', () => {
 })
 
 editProfile.addEventListener('click', () => {
-    openPopup(popupBio);
-    bioName.value = author.textContent.trim();
-    bioJob.value = job.textContent.trim();
+    openPopup(popupProfile);
+    profileName.value = author.textContent.trim();
+    profileJob.value = job.textContent.trim();
 });
 
-formElement.addEventListener('submit', (e) => {
+profileForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    author.textContent = bioName.value;
-    job.textContent = bioJob.value;
-    closePopup()
+    author.textContent = profileName.value;
+    job.textContent = profileJob.value;
+    closePopup(popupProfile)
 });
