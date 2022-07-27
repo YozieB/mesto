022380461079644ea9766e41915c4852e5f69d4e
yozieee <mrfreeze987@gmail.com>
2022-07-27 20:47:50 +0300

    fix: Фикс удаления слушателя событий для кнопки Esc

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -7,12 +7,12 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add('popup_opened')
-    document.addEventListener('keydown', e => this._handleEscClose(e))
+    document.addEventListener('keydown', this._handleEscClose)
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened')
-    document.removeEventListener('keydown', e => this._handleEscClose(e))
+    document.removeEventListener('keydown', this._handleEscClose)
   }
 
   _handleEscClose(e) {
